e933e76c3b57d4e009072808720b42bc41639da8
lysakowski-art <lysakowski.michal@gmail.com>
2020-05-07 12:53:45 +0200

    dodałem sortowanie

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,6 +3,23 @@ import Task from "./Task";
 
 const TaskList = (props) => {
   const active = props.tasks.filter((task) => task.active);
+  const done = props.tasks.filter((task) => !task.active);
+
+  if (active.length >= 2) {
+    active.sort((a, b) => {
+      a = a.text.toLowerCase();
+      b = b.text.toLowerCase();
+
+      if (a < b) return -1;
+      if (a > b) return 1;
+      return 0;
+    });
+  }
+
+  if (done.length >= 2) {
+    done.sort((a, b) => b.finishDate - a.finishDate);
+  }
+
   const activeTasks = active.map((task) => (
     <Task
       key={task.id}
@@ -12,7 +29,6 @@ const TaskList = (props) => {
     />
   ));
 
-  const done = props.tasks.filter((task) => !task.active);
   const doneTasks = done.map((task) => (
     <Task
       key={task.id}
@@ -30,8 +46,14 @@ const TaskList = (props) => {
       </div>
       <hr />
       <div className="done">
-        <h3>Lista zrobionych <em>({done.length})</em></h3>
-        {done.length >4 && <span style={{fontSize: "10px"}}>wyświetlone są tylko 4 ostatnio wykonane zadania</span>}
+        <h3>
+          Lista zrobionych <em>({done.length})</em>
+        </h3>
+        {done.length > 4 && (
+          <span style={{ fontSize: "10px" }}>
+            wyświetlone są tylko 4 ostatnio wykonane zadania
+          </span>
+        )}
         {doneTasks.slice(0, 4)}
       </div>
     </>
