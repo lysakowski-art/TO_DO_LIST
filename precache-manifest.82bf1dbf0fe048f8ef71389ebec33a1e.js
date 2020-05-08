97d0d93c748e1b1976ae6d7628379b4fc34d347d
lysakowski-art <lysakowski.michal@gmail.com>
2020-05-08 16:04:03 +0200

    Updates

---
+++ precache-manifest.82bf1dbf0fe048f8ef71389ebec33a1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd0f76abc28e761fdb1d04eff0977651",
+    "url": "/TO_DO_LIST/index.html"
+  },
+  {
+    "revision": "23be2019ddea165bf29a",
+    "url": "/TO_DO_LIST/static/css/main.f93314fe.chunk.css"
+  },
+  {
+    "revision": "386757f11db37abca1a6",
+    "url": "/TO_DO_LIST/static/js/2.df0c689a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TO_DO_LIST/static/js/2.df0c689a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23be2019ddea165bf29a",
+    "url": "/TO_DO_LIST/static/js/main.0f9f8bc2.chunk.js"
+  },
+  {
+    "revision": "5c36e941bb2f998f9ea7",
+    "url": "/TO_DO_LIST/static/js/runtime-main.44be4966.js"
+  },
+  {
+    "revision": "111d833ac9e4fa93ab4fdf84ea943e23",
+    "url": "/TO_DO_LIST/static/media/yoda.111d833a.png"
+  }
+]);
